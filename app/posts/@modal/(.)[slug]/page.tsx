545ec9b2c7c2f47f50ae4c721488c6228d99cdce
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2023-12-06 18:32:07 +0900

    Intercepting Routes 改善（catch-all 追加）

--- app/posts/@modal/(.)[slug]/page.tsx
+++ app/posts/@modal/(.)[slug]/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { Post } from '../../[slug]/_components/post';
-import type { PageProps } from '../../[slug]/types';
+import { Post } from '@/app/posts/[slug]/_components/post';
+import type { PageProps } from '@/app/posts/[slug]/types';
 
 export default function PostModal({ params }: PageProps) {
   const router = useRouter();
@@ -12,6 +13,9 @@ export default function PostModal({ params }: PageProps) {
     <dialog className="modal modal-open">
       <div className="modal-box w-5/6 max-w-none rounded-none">
         <Post postId={postId} />
+        <div className="space-x-4">
+          <Link href={`/posts/${postId}/edit`}>edit</Link>
+        </div>
 
         <button
           className="btn-ghost btn btn-sm btn-circle absolute right-2 top-2"
